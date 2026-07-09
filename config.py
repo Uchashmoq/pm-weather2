75c5c4cd1fca0e45e345269b0a6e996fa5e3c539
纯真浩一郎 <72e1bcgg@gmail.com>
2026-07-09 21:23:43 +0800

    add more attributes

--- config.py
+++ config.py
@@ -1,14 +1,18 @@
 CITY = [
     {
         "name": "Chongqing",
-        "lat": 29.75,
-        "lon": 106.75,
+        "lat": 29.57,
+        "lon": 106.55,
+        "timezone": "UTC+8",
         "models": [
             {"name": "ecmwf_aifs025_ensemble", "update_seconds": 6 * 3600},
-            {"name": "ecmwf_ifs025_ensemble", "update_seconds": 6 * 3600},
         ],
         "wunder_url": "https://www.wunderground.com/weather/cn/chongqing/ZUCK",
+        "temp_unit": "C",
+        "questions": range(28, 43),  # 30: 30 or below, 40: 40 or above, 37: 37<=T<38
     }
 ]
 
 FETCH_WUNDERGROUND_INTARVAL = 150
+FETCH_FORECAST_INTERVAL = 120
+UPDATE_PREDICTION_INTERVAL = 600
