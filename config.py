395da95d20967cff5b744bb3640b99087d42b218
纯真浩一郎 <72e1bcgg@gmail.com>
2026-07-10 22:31:53 +0800

    add chongqing center

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 CITY = [
     {
-        "name": "Chongqing",
+        "name": "Chongqing-ZUCK",
         "lat": 29.72,
         "lon": 106.63,
         "timezone": "UTC+8",
@@ -10,7 +10,19 @@ CITY = [
         "wunder_url": "https://www.wunderground.com/weather/cn/chongqing/ZUCK",
         "temp_unit": "C",
         "questions": range(28, 43),  # 30: 30 or below, 40: 40 or above, 37: 37<=T<38
-    }
+    },
+    {
+        "name": "Chongqing-center",
+        "lat": 29.57,
+        "lon": 106.55,
+        "timezone": "UTC+8",
+        "models": [
+            {"name": "ecmwf_aifs025_ensemble", "update_seconds": 6 * 3600},
+        ],
+        "wunder_url": "https://www.wunderground.com/weather/cn/chongqing/ZUCK",
+        "temp_unit": "C",
+        "questions": range(28, 43),  # 30: 30 or below, 40: 40 or above, 37: 37<=T<38
+    },
 ]
 
 FETCH_WUNDERGROUND_INTARVAL = 150
